22c8d93857346edff4467b5846e2100710fdca23
markcharyk <markcharyk@gmail.com>
2014-03-12 17:20:32 -0700

    WIP adding second day steps

--- microblog.py
+++ microblog.py
@@ -3,8 +3,10 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
+from flask import abort, render_template
 
 app = Flask(__name__)
+app.testing = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///microblog'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -28,6 +30,10 @@ class Post(db.Model):
         return '<Post %r>' % self.title
 
 
+# class User(db.Model):
+#     pass
+
+
 def write_post(title, text):
     new_post = Post(title, text)
     db.session.add(new_post)
@@ -48,5 +54,30 @@ def read_post(id):
     raise IndexError
 
 
+@app.route('/')
+def list_view():
+    posts = read_posts()
+    return render_template('list_page.html', list=posts)
+
+
+@app.route('/post/<id>')
+def perma_view(id):
+    try:
+        return render_template('permalink_page.html', post=read_post(id))
+    except IndexError:
+        abort(404)
+
+
+@app.route('/new')
+def add_post():
+    pass
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('not_found.html'), 404
+
+
 if __name__ == '__main__':
-    manager.run()
+    # db.create_all()
+    app.run()
--- tests.py
+++ tests.py
@@ -1,13 +1,15 @@
 import unittest
 from datetime import datetime
-from microblog import db, Post, write_post, read_posts, read_post
+from microblog import app, db, Post, write_post, read_posts, read_post
+from microblog import list_view, perma_view, add_post
+from werkzeug.exceptions import NotFound
 
 
 class TestWritePost(unittest.TestCase):
     def setUp(self):
         db.create_all()
 
-    def testOne(self):
+    def testWriteOne(self):
         expected = Post(u"First Post", u"""
             The text containing the first post in the blog.""")
         write_post(u"First Post", u"""
@@ -26,7 +28,7 @@ class TestReadPosts(unittest.TestCase):
     def setUp(self):
         db.create_all()
 
-    def testTwo(self):
+    def testReadOne(self):
         expected = Post(u"New Post", u"""
             The text containing the newest post in the blog.""")
         write_post(u"New Post", u"""
@@ -52,7 +54,7 @@ class TestReadPost(unittest.TestCase):
         with self.assertRaises(IndexError):
             read_post(1)
 
-    def testThree(self):
+    def testFirst(self):
         write_post(u"Newer Post", u"""
             The text containing the newer post in the blog.""")
         self.assertIsInstance(read_post(1), Post)
@@ -60,6 +62,66 @@ class TestReadPost(unittest.TestCase):
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+
+
+class TestListPage(unittest.TestCase):
+    def setUp(self):
+        db.create_all()
+
+    def testEmpty(self):
+        with app.test_request_context():
+            expected = ''
+            actual = list_view().split('div>')[1][:-2].strip()
+            self.assertEqual(expected, actual)
+
+    def testOne(self):
+        with app.test_request_context():
+            write_post(u"Post Title", u"A generic blog post")
+            expected = read_posts()[0].title
+            actual = list_view().split('h2>')[1][:-2]
+            self.assertEqual(expected, actual)
+
+    def testMany(self):
+        with app.test_request_context():
+            write_post(u"Post Number One", u"Some text that makes up blog post number one")
+            write_post(u"Post Number Two", u"Some text that makes up blog post number two")
+            write_post(u"Post Number Three", u"Some text that makes up blog post number three")
+            expected = [read_posts()[0].title, read_posts()[1].title, read_posts()[2].title]
+            actual = [list_view().split('h2>')[1][:-2], list_view().split('h2>')[3][:-2], list_view().split('h2>')[5][:-2]]
+            self.assertEqual(expected, actual)
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+
+class TestPermaPage(unittest.TestCase):
+    def setUp(self):
+        db.create_all()
+
+    def testEmpty(self):
+        with app.test_request_context():
+            with self.assertRaises(NotFound):
+                return perma_view(1)
+
+    def testOne(self):
+        with app.test_request_context():
+            write_post(u"Single Post", u"Bloggity blog post, just one this time")
+            title = perma_view(1).split('h2>')[1][:-2]
+            self.assertEqual(u"Single Post", title)
+
+    def testMany(self):
+        with app.test_request_context():
+            write_post(u"Post One", u"The first blog post in a list")
+            write_post(u"Post Two", u"The second blog post in a list")
+            write_post(u"Post Three", u"The third blog post in a list")
+            title = perma_view(3).split('h2>')[1][:-2]
+            self.assertEqual(u"Post Three", title)
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
 
 if __name__ == '__main__':
     unittest.main()
